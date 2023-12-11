bcf28dd897eb587f3d2be19b6c0c4f86a59ca256
huangxin <huangxin1@baseus.com>
2023-12-11 15:10:15 +0800

    在Excel导入功能中添加数据验证和错误处理

--- src/main/java/com/huangxin/excel/WriteErrUtil.java
+++ src/main/java/com/huangxin/excel/WriteErrUtil.java
@@ -1,5 +1,6 @@
 package com.huangxin.excel;
 
+import cn.hutool.core.collection.CollUtil;
 import cn.hutool.core.io.FileUtil;
 import cn.hutool.core.util.IdUtil;
 import com.alibaba.excel.EasyExcel;
@@ -7,10 +8,9 @@ import com.huangxin.excel.handle.importHandle.ErrMsgWriter;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 /**
  * WriteErrUtil
@@ -19,7 +19,34 @@ import java.util.Optional;
  */
 public class WriteErrUtil {
 
-    public static void writeErrMsgAndUpload(File sourceFile, int titleNum, Map<Integer, List<String>> errorMap) {
+    /**
+     * 给导入excel追加错误信息.
+     *
+     * @param sourceFile 导入的excel文件.
+     * @param titleNum   表头行数索引.
+     * @param errColl    错误信息集合.
+     * @param row        行号函数.
+     * @param errMsg     错误信息函数.
+     * @param <T>        错误信息对象.
+     * @return 上传文件的URL.
+     */
+    public static <T> String writeErrMsgAndUpload(File sourceFile, int titleNum, Collection<T> errColl, Function<T, Integer> row, Function<T, ?> errMsg) {
+        if (CollUtil.isNotEmpty(errColl)) {
+            Map<Integer, ?> errMap = errColl.stream().collect(Collectors.toMap(row, errMsg));
+            return writeErrMsgAndUpload(sourceFile, titleNum, errMap);
+        }
+        return null;
+    }
+
+    /**
+     * 给导入excel追加错误信息.
+     *
+     * @param sourceFile 导入的excel文件.
+     * @param titleNum   表头行数索引.
+     * @param errorMap   错误信息(key-行数，value-错误信息的字符串或字符串集合（string or List<String>）).
+     * @return 上传文件的URL.
+     */
+    public static String writeErrMsgAndUpload(File sourceFile, int titleNum, Map<Integer, ?> errorMap) {
         if (!errorMap.isEmpty()) {
             File errMsgFile = null;
             try {
@@ -31,7 +58,7 @@ public class WriteErrUtil {
                         .registerWriteHandler(new ErrMsgWriter(errorMap, titleNum))
                         .doWrite(new ArrayList<>());
                 // 上传
-
+                return null;
             } catch (IOException e) {
                 throw new RuntimeException(e);
             } finally {
