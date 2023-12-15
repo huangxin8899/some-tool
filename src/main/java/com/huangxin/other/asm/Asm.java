42d253f4e016af8a2103ec5a869500277e213b31
huangxin <huangxin1@baseus.com>
2023-12-15 15:24:35 +0800

    修复个bug

--- src/main/java/com/huangxin/other/asm/Asm.java
+++ src/main/java/com/huangxin/other/asm/Asm.java
@@ -1,4 +1,14 @@
 package com.huangxin.other.asm;
 
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.*;
+
+@Inherited
+@Target({METHOD, TYPE, ANNOTATION_TYPE})
+@Retention(RetentionPolicy.RUNTIME)
 public @interface Asm {
 }
